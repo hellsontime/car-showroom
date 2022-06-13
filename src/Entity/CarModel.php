5839832f6b69581ac55bd0c674af745cc2ace073
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-13 11:25:00 +0300

    CarModel entity implementation + Repository

---
+++ src/Entity/CarModel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarModelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: "vehicles_directory")]
+#[ORM\Entity(repositoryClass: CarModelRepository::class)]
+class CarModel
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 32)]
+    private string $model;
+
+    #[ORM\Column(type: 'smallint')]
+    private int $year;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+}
