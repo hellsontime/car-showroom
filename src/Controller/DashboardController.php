6b080cfb71c172d9ce6a917bdf4cd3e40d35fe9b
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-15 16:18:51 +0300

    DashboardController table related method skeletons implementation

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -77,18 +77,72 @@ class DashboardController extends BaseController
         );
     }
 
-    public function carsSoldLastYear(): Response
+    public function carsSoldLastYear(bool $sliced): Response
     {
-        return $this->render('dashboard/cars-sold-last-year.html.twig');
+        $carsSoldLastYear = [
+            ['date' => '01.05.2022', 'sold' => 15],
+            ['date' => '02.05.2022', 'sold' => 19],
+            ['date' => '03.05.2022', 'sold' => 21],
+            ['date' => '04.05.2022', 'sold' => 11],
+            ['date' => '05.05.2022', 'sold' => 14],
+            ['date' => '06.05.2022', 'sold' => 17],
+            ['date' => '07.05.2022', 'sold' => 13],
+            ['date' => '08.05.2022', 'sold' => 26],
+        ];
+
+        if ($sliced) {
+            $carsSoldLastYear = array_slice($carsSoldLastYear, 0, 5);
+        }
+
+        return $this->render(
+            'dashboard/table/cars-sold-last-year.html.twig',
+            ['carsSoldLastYear' => $carsSoldLastYear]
+        );
     }
 
-    public function unsoldCars(): Response
+    public function unsoldCars(bool $sliced): Response
     {
-        return $this->render('dashboard/unsold-cars.html.twig');
+        $unsoldCars = [
+            ['model' => 'civic', 'year' => 2005, 'price' => 1234, 'color' => 'color'],
+            ['model' => 'lamborghini', 'year' => 2003, 'price' => 41234, 'color' => 'color'],
+            ['model' => 'skoda', 'year' => 1994, 'price' => 12345, 'color' => 'color'],
+            ['model' => 'volkswagen', 'year' => 2017, 'price' => 1234, 'color' => 'color'],
+            ['model' => 'audi', 'year' => 2018, 'price' => 1234, 'color' => 'color'],
+            ['model' => 'bmw', 'year' => 2022, 'price' => 1234, 'color' => 'color'],
+            ['model' => 'mercedes', 'year' => 1999, 'price' => 12325, 'color' => 'color'],
+            ['model' => 'porsche', 'year' => 2001, 'price' => 123442, 'color' => 'color'],
+        ];
+
+        if ($sliced) {
+            $unsoldCars = array_slice($unsoldCars, 0, 5);
+        }
+
+        return $this->render(
+            'dashboard/table/unsold-cars.html.twig',
+            ['unsoldCars' => $unsoldCars]
+        );
     }
 
-    public function carsCurrentlyOnSale(): Response
+    public function carsCurrentlyOnSale(bool $sliced): Response
     {
-        return $this->render('dashboard/cars-currently-on-sale.html.twig');
+        $carsCurrentlyOnSale = [
+            ['model' => 'mercedes', 'year' => 2016, 'price' => 12325, 'color' => 'color'],
+            ['model' => 'lamborghini', 'year' => 2003, 'price' => 41234, 'color' => 'color'],
+            ['model' => 'skoda', 'year' => 1994, 'price' => 12345, 'color' => 'color'],
+            ['model' => 'volkswagen', 'year' => 2017, 'price' => 1234, 'color' => 'color'],
+            ['model' => 'porsche', 'year' => 2001, 'price' => 123442, 'color' => 'color'],
+            ['model' => 'audi', 'year' => 2018, 'price' => 1234, 'color' => 'color'],
+            ['model' => 'bmw', 'year' => 2022, 'price' => 1234, 'color' => 'color'],
+            ['model' => 'civic', 'year' => 2005, 'price' => 1234, 'color' => 'color'],
+        ];
+
+        if ($sliced) {
+            $carsCurrentlyOnSale = array_slice($carsCurrentlyOnSale, 0, 5);
+        }
+
+        return $this->render(
+            'dashboard/table/cars-currently-on-sale.html.twig',
+            ['carsCurrentlyOnSale' => $carsCurrentlyOnSale]
+        );
     }
 }
