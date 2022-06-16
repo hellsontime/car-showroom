b17a24903551436cc440f557636b19973196cc99
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-16 16:22:18 +0300

    CarShowroomRepository get average methods implementation

--- src/Repository/CarShowroomRepository.php
+++ src/Repository/CarShowroomRepository.php
@@ -39,6 +39,49 @@ class CarShowroomRepository extends ServiceEntityRepository
         }
     }
 
+    public function getAveragePriceAllTime(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->select('avg(c.price), count(c)')
+            ->andWhere('c.date_of_sale IS NOT NULL')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getAveragePriceToday(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->select('avg(c.price), count(c)')
+            ->andWhere('c.date_of_sale = :today')
+            ->setParameter('today', date('Y-m-d'))
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getAveragePriceInRange(int $daysAgo): array
+    {
+        return $this->createQueryBuilder('c')
+            ->select('avg(c.price), count(c)')
+            ->andWhere('c.date_of_sale BETWEEN :from AND :to')
+            ->setParameter('from', date('Y-m-d', strtotime('-'.$daysAgo.' day')))
+            ->setParameter('to', date('Y-m-d'))
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getCarsSoldLastYear(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->select('c.date_of_sale AS date, COUNT(c.date_of_sale) AS sold')
+            ->andWhere('c.date_of_sale BETWEEN :from AND :to')
+            ->groupBy('c.date_of_sale')
+            ->orderBy('c.date_of_sale', 'DESC')
+            ->setParameter('from', date('Y-m-d', strtotime('-365 day')))
+            ->setParameter('to', date('Y-m-d'))
+            ->getQuery()
+            ->getResult();
+    }
+
 //    /**
 //     * @return CarShowroom[] Returns an array of CarShowroom objects
 //     */
