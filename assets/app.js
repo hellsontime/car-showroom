8dc5c85b39a04b09bfa8e66d304c18c953f496cb
Raman Kanapeshka <raman.kanapeshka@innowise-group.com>
2022-06-17 16:59:15 +0300

    toggle sidebar js script implementation

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,43 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+// // start the Stimulus application
+// import './bootstrap';
+
+
+// work with sidebar
+var btn     = document.getElementById('sliderBtn'),
+    sideBar = document.getElementById('sideBar'),
+    sideBarHideBtn = document.getElementById('sideBarHideBtn');
+
+// show sidebar
+btn.addEventListener('click' , function(){
+    if (sideBar.classList.contains('md:-ml-64')) {
+        sideBar.classList.replace('md:-ml-64' , 'md:ml-0');
+        sideBar.classList.remove('md:slideOutLeft');
+        sideBar.classList.add('md:slideInLeft');
+    };
+});
+
+// hide sideBar
+sideBarHideBtn.addEventListener('click' , function(){
+    if (sideBar.classList.contains('md:ml-0' , 'slideInLeft')) {
+        var _class = function(){
+            sideBar.classList.remove('md:slideInLeft');
+            sideBar.classList.add('md:slideOutLeft');
+
+        };
+        var animate = async function(){
+            await _class();
+
+            setTimeout(function(){
+                sideBar.classList.replace('md:ml-0' , 'md:-ml-64');
+            } , 300);
+
+        };
+
+        _class();
+        animate();
+    };
+});
+// end with sidebar
